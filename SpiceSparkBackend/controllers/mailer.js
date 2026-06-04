4cfdfe74dcdedb09c7ea3883316d8d5362dbf93a
Saurav Sharma <saurav.sharma@tietoevry.com>
2026-06-05 00:26:21 +0200

    Changes for Integration

--- SpiceSparkBackend/controllers/mailer.js
+++ SpiceSparkBackend/controllers/mailer.js
@@ -4,8 +4,8 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 const transporter = nodemailer.createTransport({
  host: process.env.SMTP_HOST,
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD, // Gmail App Password
